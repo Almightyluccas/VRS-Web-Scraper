61d638b230e6260ea420034ba38c807b32e01844
Luccas Amorim <117477920+Almightyluccas@users.noreply.github.com>
2025-05-03 21:55:11 -0300

    Remove commented-out code for garage61 upload in index.ts

--- src/bots/garage61-scraper.ts
+++
@@ -1,5 +0,0 @@
-export const uploadToGarage61 = async () => {
-  const x = 2
-  if (x != 2) throw NaN
-  return 'x'
-}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { vrsScraper } from './bots/vrs-scraper';
-//import { uploadToGarage61 } from './bots/garage61-scraper';
 import {tryCatch} from "./utils/tryCatch";
 
 
 const main = async () => {
   const {data: vrsData, error: vrsError} = await tryCatch(vrsScraper());
   if (vrsError) return {vrsError: vrsError};
-
-  // const {data: data, error: garage61Error} = await tryCatch(uploadToGarage61());
-  // if(garage61Error) return {garage61Error: "Unable to upload to Garage61"};
 
   return {success: true};
 }
